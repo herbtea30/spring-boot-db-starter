ecbbcecb5f2ae796001b15c051cd24eb7ba023be
herbtea <herbtea30@gmail.com>
2019-04-09 22:49:55 +0900

    version 1.00

---
+++ src/main/java/com/herbtea/study/springbootdbstarter/SpringBootDbStarterApplication.java
@@ -0,0 +1,12 @@
+package com.herbtea.study.springbootdbstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDbStarterApplication.class, args);
+	}
+}
